5fd98914112fd0987634999ee0f8f887560fd1b0
CardKwando <13jthanm@gmail.com>
2024-05-25 12:03:19 -0400

    modify: App.css + App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import React from 'react';
+import './App.css';
+
 function App() {
   return (
-    <h1>codebreaker</h1>
-    <h2><marquee>This page is under construction</marquee></h2>
-    <h4>more stuff will be coming soon</h4>
-    <h4>look, this is our mascot, void the cat 😼 </h4>
-    <img src="maine-coon-694730_1920.jpg" alt="pic of void" width="40%">
-    	
-    <a style="color:white" href="https://CardKwando.github.io/testing/subFolder/" >
-    	i
-    </a>
-  )
+    <div>
+      <h1>codebreaker</h1>
+      <div className="marquee">This page is under construction</div>
+      <h4>more stuff will be coming soon</h4>
+      <h4>look, this is our mascot, void the cat 😼 </h4>
+      <img src="maine-coon-694730_1920.jpg" alt="pic of void" width="40%"/>
+      <a style={{color: "white"}} href="https://CardKwando.github.io/testing/subFolder/">
+        i
+      </a>
+    </div>
+  );
 }
-
-export default App;
